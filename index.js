1bf4cb25e9168cbf5dcb25798cdac4942a34fe20
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-28 21:48:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * expand-task <https://github.com/jonschlinkert/expand-task>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
